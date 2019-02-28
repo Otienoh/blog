44d904a30619013a3dd63134ad06f93c62e69160
Otienoh <otienojulie@gmail.com>
2019-02-28 14:56:04 +0300

    Enable registration so I can register myself 👀

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'BlogController@index')->name('blog.index');
 
-Auth::routes(['register' => false]);
+Auth::routes(['register' => true]);
 
 Route::prefix('blog')->group(function () {
     Route::middleware('Canvas\Http\Middleware\ViewThrottle')->get('/{slug}', 'BlogController@post')->name('blog.post');
